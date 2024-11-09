050e48715957a1baf22805847d1fe0f3e55cd87f
schmisev <schmisev@gmail.com>
2024-11-09 18:58:41 +0100

    some morge changes to the package

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin
+const { webpack } = require('webpack');
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -23,9 +24,13 @@ module.exports = {
     },
     optimization: {
         usedExports: true, // <- remove unused function
+        minimize: true,
+        minimizer: [new TerserPlugin()]
     },
     devServer: {
         static: './dist',
     },
-    plugins: [new BundleAnalyzerPlugin()]
+    plugins: [
+        // new BundleAnalyzerPlugin(),
+    ]
 };
