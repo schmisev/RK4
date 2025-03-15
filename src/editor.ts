79b361980034deb7c5f5e4a3b91872ae01b795f6
schmisev <schmisev@gmail.com>
2025-03-15 19:01:27 +0100

    basic functionality implemented, no resizing yet

--- src/editor.ts
+++ src/editor.ts
@@ -21,8 +21,8 @@ const dummyTask = STD_TASKS[DEFAULT_TASK];
 let env: WorldViewEnv;
 env = {
   isRunning: false,
-  dt: 0,
-  maxDt: 0,
+  dt: 10,
+  maxDt: 10,
   manualMode: false,
   objOverlay,
   playState,
@@ -39,11 +39,8 @@ env = {
 const descriptionEditor = ace.edit("task-description", {
   minLines: 1,
   value: "",
-  mode: "ace/mode/html",
-  theme: "ace/theme/monokai",
   //readOnly: true,
   showPrintMargin: false,
-  enableBasicAutocompletion: true
 });
 
 const preloadEditor = ace.edit("preload-editor", {
@@ -56,3 +53,111 @@ const preloadEditor = ace.edit("preload-editor", {
 });
 
 let robotView = setupRobotView(env);
+
+// world editor
+interface WorldProxy {
+  L: number,
+  W: number,
+  H: number,
+  fields: string[][],
+}
+
+let worldIndex: number = 0;
+let worldProxies: WorldProxy[] = [
+  {
+    L: 4, W: 3, H: 6,
+    fields: [
+      ["S_", "_", "_", "_"],
+      ["_", "_", "_", "_"],
+      ["_", "_", "_", "_"],
+    ]
+  },
+  {
+    L: 3, W: 5, H: 4,
+    fields: [
+      ["S_", "_", "_"],
+      ["_", "_", "_"],
+      ["_", "_", "_"],
+      ["_", "_", "_"],
+      ["_", "_", "_"],
+    ]
+  },
+]
+
+document.getElementById("next-index")!.onclick = () => {
+  worldIndex = (worldIndex + 1) % worldProxies.length;
+  reloadWorld();
+}
+document.getElementById("prev-index")!.onclick = () => {
+  worldIndex = (worldIndex + worldProxies.length - 1) % worldProxies.length;
+  reloadWorld();
+}
+
+function buildWorldEdit(worldProxy: WorldProxy, container: HTMLDivElement, callback: () => void) {
+  let wrapper = document.createElement("div") as HTMLDivElement;
+
+  for (let key of ["L", "W", "H"] as (keyof WorldProxy)[]) {
+    let keyEdit = document.createElement("input");
+    keyEdit.classList.add("key-edit");
+    keyEdit.type = "number";
+    keyEdit.value = worldProxy[key].toString();
+
+    // dim editing
+    keyEdit.onchange = (evt: Event) => {
+      worldProxy[key] = parseInt(keyEdit.value) as any;
+      callback();
+    }
+
+    wrapper.appendChild(keyEdit);
+  }
+
+  let worldTable = document.createElement("table") as HTMLTableElement;
+  worldTable.classList.add("world-table");
+
+  let w = worldProxy.fields;
+  for (let r of w) {
+    let row = document.createElement("tr") as HTMLTableRowElement;
+    for (let [i, f] of r.entries()) {
+      let field = document.createElement("td") as HTMLTableCellElement;
+      field.classList.add("world-field");
+
+      let fieldEdit = document.createElement("input") as HTMLInputElement;
+      fieldEdit.value = f;
+
+      // field editing
+      fieldEdit.onchange = (evt: Event) => {
+        r[i] = fieldEdit.value;
+        callback();
+      }
+
+      field.appendChild(fieldEdit);
+      row.appendChild(field);
+    }
+    worldTable.appendChild(row);
+  }
+  wrapper.appendChild(worldTable);
+  container.appendChild(wrapper);
+}
+
+function reloadWorldEdit(worldProxies: WorldProxy[], id: string, callback: () => void) {
+  let container = document.getElementById(id) as HTMLDivElement;
+  if (!container) return;
+  container.replaceChildren();
+
+  for (let w of worldProxies) {
+    buildWorldEdit(w, container, callback);
+  }
+}
+
+function reloadWorld(): void {
+  let genStr = "";
+  for (let i = 0; i < worldProxies.length; i++) {
+    let world = worldProxies[i];
+    genStr += `x;${world.L};${world.W};${world.H};\n`
+    genStr += world.fields.map((r) => r.join(";")).join("\n");
+  }
+  env.world = new World(genStr, worldIndex);
+}
+
+reloadWorldEdit(worldProxies, "world-edit-container", reloadWorld);
+reloadWorld();
